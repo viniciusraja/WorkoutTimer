f839904e7217dcd099cd34a58cf21448d0116296
Vinícius Rajá <viniciusrfl@hotmail.com>
2020-09-03 21:49:07 -0300

    Add: MyActionsScreen

--- src/screens/MyActionsScreen/styles.js
+++ src/screens/MyActionsScreen/styles.js
@@ -8,15 +8,14 @@ export const styles = StyleSheet.create({
     paddingHorizontal: 10,
     backgroundColor: Constants.Colors.backgroundColor,
   },
-  actionsList:{
-    width:Constants.Layout.window.width,
-    height:'100%'
+  actionsList: {
+    width: Constants.Layout.window.width,
+    height: '100%',
   },
-  titleLastWorkouts:{
-    marginBottom:10,
+  titleLastWorkouts: {
+    marginBottom: 10,
     fontFamily: Constants.Fonts.primaryFontFamily,
     fontSize: Constants.Fonts.mediumFontSize,
     color: Constants.Colors.primaryText,
   },
-  
 });
