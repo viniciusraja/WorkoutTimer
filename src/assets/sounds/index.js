634cb304094a54064a8b52fa855014469e4da931
Vinícius Rajá <viniciusrfl@hotmail.com>
2020-09-06 17:00:19 -0300

    Add: Sound Effects Assets

---
+++ src/assets/sounds/index.js
@@ -0,0 +1,32 @@
+const Applauding = require("assets/sounds/Applauding.mp3");
+const Boxing_Bell = require("assets/sounds/Boxing_Bell.mp3");
+const Cello = require("assets/sounds/Cello.mp3");
+const Chime_High = require("assets/sounds/Chime_High.mp3");
+const Chime_Low = require("assets/sounds/Chime_Low.mp3");
+const Chime_Tink = require("assets/sounds/Chime_Tink.mp3");
+const Clarinet = require("assets/sounds/Clarinet.mp3");
+const Go = require("assets/sounds/Go.mp3");
+const Horn_Hit_Loud = require("assets/sounds/Horn_Hit_Loud.mp3");
+const HornBulb = require("assets/sounds/HornBulb.mp3");
+const Horns = require("assets/sounds/Horns.mp3");
+const Ride_Cymbal = require("assets/sounds/Ride_Cymbal.mp3");
+const Trumpets = require("assets/sounds/Trumpets.mp3");
+const Whistle_Hi = require("assets/sounds/Whistle_Hi.mp3");
+const soundsAssetsPathArray = [
+  { name: 'Applauding', path: Applauding },
+  { name: 'BoxingBell', path: Boxing_Bell },
+  { name: 'Cello', path: Cello },
+  { name: 'Chime High', path: Chime_High },
+  { name: 'Chime Low', path: Chime_Low },
+  { name: 'Chime Tink', path: Chime_Tink },
+  { name: 'Clarinet', path: Clarinet },
+  { name: 'Go', path: Go },
+  { name: 'Horn Hit Loud', path: Horn_Hit_Loud },
+  { name: 'HornBulb', path: HornBulb },
+  { name: 'Horns', path: Horns },
+  { name: 'Ride Cymbal', path: Ride_Cymbal },
+  { name: 'Trumpets', path: Trumpets },
+  { name: 'Whistle Hi', path: Whistle_Hi },
+];
+
+export default soundsAssetsPathArray;
